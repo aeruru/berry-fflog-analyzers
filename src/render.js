cd8f71201516992815ef14e8b7593bd3aa65d0cf
cody-berry <cody.tian@icloud.com>
2026-07-28 18:16:45 -0700

    correct the P2 DPS link

--- src/render.js
+++ src/render.js
@@ -176,7 +176,7 @@ function renderZoneFightCards(report, fights, { activeFightEventKey, fightEventD
                 ${report.reportCode ? `<a class="fflogs-fight-link" href="${escapeHtml(getFflogsFightUrl(report.reportCode, fight.id))}" target="_blank" rel="noreferrer">FFLogs</a>` : ''}
                 ${showArrowsAnalyzer ? `<a class="analyzer-link" href="${escapeHtml(getArrowAnalyzerUrl(report.reportCode, fight.id))}" target="_blank" rel="noreferrer">Arrows analyzer</a>` : ''}
                 ${showP2Analyzer ? `<a class="analyzer-link" href="${escapeHtml(getForsakenAnalyzerUrl(report.reportCode, fight.id))}" target="_blank" rel="noreferrer">P2 analyzer</a>` : ''}
-                ${showP2DPS ? `<a class="analyzer-link" href="${escapeHtml(getForsakenAnalyzerUrl(report.reportCode, fight.id, fight.startOffsetMs))}" target="_blank" rel="noreferrer">P2 DPS</a>` : ''}
+                ${showP2DPS ? `<a class="analyzer-link" href="${escapeHtml(getP2DPSUrl(report.reportCode, fight.id, fight.startOffsetMs))}" target="_blank" rel="noreferrer">P2 DPS</a>` : ''}
                 <button class="cache-clear-button fight-cache-clear" data-report-id="${escapeHtml(report.id)}" data-fight-id="${escapeHtml(fight.id)}" type="button">Clear cache</button>
               </div>
             </div>
